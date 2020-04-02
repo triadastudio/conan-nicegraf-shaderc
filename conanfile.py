4dd0e5885f0ed02f566c253fdd13e360925ffc1a
Bagrat Dabaghyan <bagrat@triadastudio.com>
2020-04-02 07:18:20 +0400

    dxc-experiment version with included dyn-libs

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class NicegrafShadercConan(ConanFile):
     name = "nicegraf-shaderc"
     short_paths = True  #windows MAX_PATH(260) limitation fix
-    version = "0.3"
+    version = "0.5b"
     license = "MIT"
     author = "<Bagrat Dabaghyan> <dbagrat@gmail.com>"
     url = "https://github.com/dBagrat/conan-nicegraf-shaderc.git"
@@ -19,8 +19,8 @@ class NicegrafShadercConan(ConanFile):
 
     def source(self):
         git = tools.Git()
-        git.clone("https://github.com/nicebyte/nicegraf-shaderc.git", "master", shallow=True)
-        git.checkout("ab8885a0caab7a4bcd6d659918b7ef61ba88e43e")
+        git.clone("https://github.com/nicebyte/nicegraf-shaderc.git", "dxc-experiment", shallow=True)
+        git.checkout("da5d2c583216990cf0ae827725197bafbf8dc978")
 
     def configure(self):
         del self.settings.compiler.runtime
@@ -38,8 +38,10 @@ class NicegrafShadercConan(ConanFile):
     def package(self):
         if self.settings.os_build == "Windows":
             self.copy("nicegraf_shaderc.exe", dst="bin", keep_path=False)
+            self.copy("*.dll", dst="bin", keep_path=False)
         else:
             self.copy("nicegraf_shaderc", dst="bin", keep_path=False)
+            self.copy("*.dylib", dst="bin", keep_path=False)
 
     def package_info(self):
         self.cpp_info.bindirs = ['bin']
