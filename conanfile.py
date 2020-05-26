eb3ecafe598709a70b64c569a0c310a20bfddbcc
Bagrat Dabaghyan <bagrat@triadastudio.com>
2020-05-26 08:07:08 +0400

    Ver 0.6.2: updated dxc with fixed <discard> op

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ import os
 class NicegrafShadercConan(ConanFile):
     name = "nicegraf-shaderc"
     short_paths = True  #windows MAX_PATH(260) limitation fix
-    version = "0.6.1"
+    version = "0.6.2"
     license = "MIT"
     author = "<Bagrat Dabaghyan> <dbagrat@gmail.com>"
     url = "https://github.com/dBagrat/conan-nicegraf-shaderc.git"
@@ -20,7 +20,7 @@ class NicegrafShadercConan(ConanFile):
     def source(self):
         git = tools.Git()
         git.clone("https://github.com/nicebyte/nicegraf-shaderc.git", "master", shallow=True)
-        git.checkout("e5c98665b460e172f464fd073c9d7f3fb5deb7a5")
+        git.checkout("42ac142af6432d48c0a82c9d066522886ce77991")
 
     def configure(self):
         del self.settings.compiler.runtime
